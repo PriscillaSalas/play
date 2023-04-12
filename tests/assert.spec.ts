88594c5cb58b722705192fea7e29eafab963ee56
PriscillaSalas <priscillasalas91@gmail.com>
2023-04-12 10:59:31 -0600

    adding new test

--- tests/assert.spec.ts
+++ tests/assert.spec.ts
@@ -1,19 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('playing with assertions', async ({ page }) => {
-
-  await page.goto('http://uitestingplayground.com/textinput');
+test("playing with assertions", async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/textinput");
 
   //verify input is visible
-  await expect(page.locator('#newButtonName')).toBeVisible();
+  await expect(page.locator("#newButtonName")).toBeVisible();
   //select input and fill the input with your text
-  await page.locator('#newButtonName').fill('Priscilla');
+  await page.locator("#newButtonName").fill("Priscilla");
   //click in button
-  await page.locator('#updatingButton').click();
+  await page.locator("#updatingButton").click();
   //verify button text updated
-  await expect(page.locator('#updatingButton')).toContainText('Priscilla');
- 
-
-
-
- });
+  await expect(page.locator("#updatingButton")).toContainText("Priscilla");
+});
--- tests/reto.spec.ts
+++ tests/reto.spec.ts
@@ -9,27 +9,53 @@
  * 8. Verifica que el modal ya no es visible ✅
  */
 
- import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
- test('reto #1', async ({ page }) => {
-   await page.goto('https://automationexercise.com/');
- 
-   await page.locator('a[href="/product_details/1"]').click();
- 
-   // close ad
-   if (page.url() === 'https://automationexercise.com/#google_vignette') {
-     await page.frameLocator('#aswift_5').frameLocator('internal:attr=[title="Advertisement"i]').getByRole('button', { name: 'Close ad' }).click();
-   }
- 
-   await page.locator('#quantity').fill('3');
- 
-   await page.locator('button:has-text("Add to cart")').click();
- 
-   await page.locator('#cartModal').isVisible();
-   await page.locator('text=Added!').isVisible();
- 
-   await page.locator('button.close-modal').click();
- 
-   await expect(page.locator('#cartModal')).not.toBeVisible();
- 
- });
+test("reto #1", async ({ page }) => {
+  await page.goto("https://automationexercise.com/");
+
+  await page.locator('a[href="/product_details/1"]').click();
+
+  // close ad
+  if (page.url() === "https://automationexercise.com/#google_vignette") {
+    await page
+      .frameLocator("#aswift_5")
+      .frameLocator('internal:attr=[title="Advertisement"i]')
+      .getByRole("button", { name: "Close ad" })
+      .click();
+  }
+
+  await page.locator("#quantity").fill("3");
+
+  await page.locator('button:has-text("Add to cart")').click();
+
+  await page.locator("#cartModal").isVisible();
+  await page.locator("text=Added!").isVisible();
+
+  await page.locator("button.close-modal").click();
+
+  await expect(page.locator("#cartModal")).not.toBeVisible();
+});
+
+test("añadir producto al carrito", async ({ page }) => {
+  /*Abre el navegador la tienda virtual
+    deesliza hasta ver los productos
+    click en ver producto
+    agregar al carrito
+    */
+  await page.goto("https://automationexercise.com/");
+
+  await page.mouse.down();
+
+  await page.mouse.up();
+
+  await page.locator("a[href='/product_details/1']").click();
+  await page.goto("https://automationexercise.com/product_details/1");
+  await page.waitForSelector("button[type='button']", { timeout: 5000 });
+  await page.locator("button[type='button']").click();
+  await page.waitForSelector(".modal-title.w-100");
+  const modalTitle = await page
+    .locator(".modal-title.w-100")
+    .evaluate((el) => el.textContent);
+  await expect(modalTitle).toContain("Added!");
+});
